ac1c0a06021453dfeee7be5a467e2798f0cd498c
Atanas Popatanasov <apopatan@progress.com>
2022-10-04 11:20:09 +0300

    Create the tab navigation menu

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Telerik.Maui.Controls.Compatibility;
 
 namespace NucleusDevConApp;
 
@@ -9,6 +9,7 @@ public static class MauiProgram
         var builder = MauiApp.CreateBuilder();
         builder
             .UseMauiApp<App>()
+            .UseTelerik()
             .ConfigureFonts(fonts =>
             {
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
