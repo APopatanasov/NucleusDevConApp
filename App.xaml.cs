e41ef59c67d6b985806c23c7a16aecd5a504c4cb
Atanas Popatanasov <apopatan@progress.com>
2022-10-04 15:49:47 +0300

    Android fixes

--- App.xaml.cs
+++ App.xaml.cs
@@ -5,7 +5,9 @@ public partial class App : Application
     public App()
     {
         InitializeComponent();
-
+#if ANDROID
+        Application.AccentColor = Color.FromArgb("#2B0B98");
+#endif
         MainPage = new NavigationPage(new MainPage());
     }
 }
